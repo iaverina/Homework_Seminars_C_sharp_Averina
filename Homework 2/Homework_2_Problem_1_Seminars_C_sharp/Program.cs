1ee837851204173f7772147cfc9d48a3de2e8233
Irina Averina <irina.averina@gmail.com>
2022-12-25 21:29:13 +0300

    second draft of the solution of problem 1 homework 2

--- Homework 2/Homework_2_Problem_1_Seminars_C_sharp/Program.cs
+++ Homework 2/Homework_2_Problem_1_Seminars_C_sharp/Program.cs
@@ -3,25 +3,31 @@
 
 // Asking for input from the user
 Console.Write("Enter a three-digit number: ");
-int number = Convert.ToInt32(Console.ReadLine());
+int InitialNumber = Convert.ToInt32(Console.ReadLine());
 int secondDigit = 0;
 
 // Checking whether the entered number is a thee-digit number
-int numberOfDigit=0;
-int count=0;
+int number=InitialNumber;
+int numberOfDigits = 0;
 
-    while (number > 0)
+while (number > 0)
         {
-            number = number / 10;
-            count++;
+            int count=0;
+            number = number % 10;
+            count ++;
+            numberOfDigits = count;
         }    
 
 //if the condition is met then we proceed to checking the second digit
-if (numberOfDigit ==3)
-    {
-        secondDigit= (number / 10) - (number / 100);
-        Console.Write($"the number od digits is, {numberOfDigit}");    }
+if (numberOfDigits ==3)
+{
+    secondDigit= (InitialNumber / 10 % 10);
+    Console.Write($"the second digit of {InitialNumber} is {secondDigit}");
+    return; 
+}
 
-else Console.Write("the number of digits is incorrect");
-return;
-
+else 
+{
+    Console.Write("the number of digits is incorrect");
+    return;
+}
