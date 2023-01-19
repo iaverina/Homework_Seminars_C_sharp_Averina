1638a3418a4150577c1a457f3309f9617d4ae54e
Irina Averina <irina.averina@gmail.com>
2023-01-19 20:44:56 +0300

    corrected 29 solution

--- Homework 4/HW4Task29/Program.cs
+++ Homework 4/HW4Task29/Program.cs
@@ -1,27 +1,28 @@
 ﻿// Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран. Данные вводятся с консоли пользователем
+Console.WriteLine("Input firstOf8");
+int firstOf8 = Convert.ToInt32(Console.ReadLine());
 
-Console.WriteLine("Input left border of array");
-int left = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Input secondOf8");
+int secondOf8 = Convert.ToInt32(Console.ReadLine());
 
-Console.WriteLine("Input left border of array");
-int left = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Input thirdOf8");
+int thirdOf8 = Convert.ToInt32(Console.ReadLine());
 
-int[] GetRandomArray(int size, int left, int right)
-{
-int[] array = new int[size];
-for(int i = 0; i < array.Length; i++)
-{
-array[i] = new Random().Next(left, right+1);
-}
-return array;
-}
-int size = 8;
-int left = -9;
-int right = 9;
-int[] massive = GetRandomArray(size, left, right);
-Console.WriteLine(string.Join(", ", massive));
-for (int i = 0; i < size; i++)
-{
-massive[i] = massive[i] * (-1);
-}
-Console.WriteLine(string.Join(", ", massive));
+Console.WriteLine("Input fourthOf8");
+int fourthOf8 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Input fifthOf8");
+int fifthOf8 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Input sixthsOof8");
+int sixthsOof8 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Input seventhOf8");
+int seventhOf8 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Input eighthOf8");
+int eighthOf8 = Convert.ToInt32(Console.ReadLine());
+
+int[] arrayOf8 = {firstOf8, secondOf8, thirdOf8, fourthOf8, fifthOf8, sixthsOof8, seventhOf8, eighthOf8};;
+
+Console.WriteLine(string.Join(", ", arrayOf8));
