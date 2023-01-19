ae99ef6e3559da483454bc786ff05f9dc68c67cd
Irina Averina <irina.averina@gmail.com>
2023-01-19 18:33:20 +0300

    task24 solution

---
+++ Homework 4/HW4Task24/Program.cs
@@ -0,0 +1,14 @@
+﻿// Задача 24: Напишите программу, которая принимает на вход число (А) и выдаёт сумму чисел от 1 до А.
+
+Console.WriteLine("Input A");
+
+int A = Convert.ToInt32(Console.ReadLine());
+int i = 0;
+int sum = 0;
+
+while (i<A)
+{ 
+    i++;
+    sum = sum + i;
+}
+Console.WriteLine (sum);
