68f7e034636c175d095e07c46f075f9ca1436088
Irina Averina <irina.averina@gmail.com>
2023-01-19 19:57:05 +0300

    solution task 29

---
+++ Homework 4/HW4Task29/Program.cs
@@ -0,0 +1,27 @@
+﻿// Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран. Данные вводятся с консоли пользователем
+
+Console.WriteLine("Input left border of array");
+int left = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Input left border of array");
+int left = Convert.ToInt32(Console.ReadLine());
+
+int[] GetRandomArray(int size, int left, int right)
+{
+int[] array = new int[size];
+for(int i = 0; i < array.Length; i++)
+{
+array[i] = new Random().Next(left, right+1);
+}
+return array;
+}
+int size = 8;
+int left = -9;
+int right = 9;
+int[] massive = GetRandomArray(size, left, right);
+Console.WriteLine(string.Join(", ", massive));
+for (int i = 0; i < size; i++)
+{
+massive[i] = massive[i] * (-1);
+}
+Console.WriteLine(string.Join(", ", massive));
