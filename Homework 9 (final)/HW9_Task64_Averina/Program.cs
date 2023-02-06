ae2b450bc8bf2be9d54105f82982a605ecaf5dd3
Irina Averina <irina.averina@gmail.com>
2023-02-06 18:39:12 +0300

    solution 64

---
+++ Homework 9 (final)/HW9_Task64_Averina/Program.cs
@@ -0,0 +1,27 @@
+﻿// Задача 64: Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от N до 1.
+
+// N = 5 -> "5, 4, 3, 2, 1"
+// N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
+
+int n = InputInt("Input and integer > 1: ");
+int m = 1;
+if (n < 1)
+{
+    Console.WriteLine("Wrong unmber");
+}
+Console.WriteLine(NaturalNumber(n, m));
+
+int NaturalNumber(int n, int m)
+{
+    if (n == m)
+        return n;
+    else
+        Console.Write($"{NaturalNumber(n, m + 1)}, ");
+    return m;
+}
+
+int InputInt(string output)
+{
+    Console.Write(output);
+    return int.Parse(Console.ReadLine());
+}
