2487e6fa464e73adf66606a5ecb932738aed5923
Irina Averina <irina.averina@gmail.com>
2023-02-06 19:22:48 +0300

    solution 68

--- Homework 9 (final)/HW9_Task66_Averina/Program.cs
+++ Homework 9 (final)/HW9_Task66_Averina/Program.cs
@@ -2,31 +2,6 @@
 // M = 1; N = 15 -> 120
 // M = 4; N = 8. -> 30
 
-// int m = InputInt ("Input M:");
-// int n = InputInt ("Input N>M:");
-// Console.WriteLine ($"Sum of elements from {m} to {n} = {CountNaturalSum(m, n)}");
-
-// if (m<n)
-// {
-//     int InputInt(string output)
-//     {
-//         Console.Write(output);
-//         return int.Parse(Console.ReadLine());
-//     }
-
-//     int CountNaturalSum(int m, int n)
-//     {
-//         if (m == n)
-//             return n;
-//         return n + CountNaturalSum(m, n - 1);
-//     }
-// }    
-
-// // else
-// // {
-// //     Console.WriteLine($"{m} is more than {n}, please, reverse the numbers");
-// // }
-
 int m = InputInt("Input M:");
 int n = InputInt("Input N>M:");
 
---
+++ Homework 9 (final)/HW9_Task68_Averina/Program.cs
@@ -0,0 +1,27 @@
+﻿// Задача 68: Напишите программу вычисления функции Аккермана с помощью рекурсии. Даны два неотрицательных числа m и n.
+//m = 2, n = 3 -> A(m,n) = 9
+
+Console.WriteLine("Input two integers: M and N.");
+int m = InputInt("Input M>=0: ");
+int n = InputInt("Input N>=0: ");
+
+int InputInt(string output)
+{
+    Console.Write(output);
+    return int.Parse(Console.ReadLine());
+}
+
+		int AkkermanFunc(int m, int n)
+		{
+			if(m == 0)
+			{
+				return n + 1;
+			}
+			if(m > 0 && n == 0)
+			{
+				return AkkermanFunc(m - 1, 1);
+			}
+			return AkkermanFunc(m - 1, AkkermanFunc(m, n - 1));
+		}
+		
+		Console.WriteLine(AkkermanFunc(m,n));
