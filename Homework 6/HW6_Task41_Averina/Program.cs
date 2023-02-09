08eec5c3ed59d4521064345af75ee9ac399366da
Irina Averina <irina.averina@gmail.com>
2023-02-09 16:48:43 +0300

    Solution 41

---
+++ Homework 6/HW6_Task41_Averina/Program.cs
@@ -0,0 +1,23 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. 
+// Посчитайте, сколько чисел больше 0 ввёл пользователь. 
+// Ввод чисел останавливается при помощи ввода стоп-слова "stop" 
+// и производится при помощи нажатия Enter
+
+// 0, 7, 8, -2, -2 -> 2
+
+// 1, -7, 567, 89, 223-> 4
+
+
+Console.Write("Input as many integers as you like dividing them with space: ");
+int[] arr = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+int count = 0;
+ 
+for (int i = 0; i < arr.Length; i++)
+{
+    if (arr[i] > 0)
+    {
+        count++;
+    }
+}
+ 
+Console.WriteLine($"The number of integers > 0 = {count}");
